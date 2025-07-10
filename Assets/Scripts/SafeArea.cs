daacc68ddc00902bc155ae30c15920d98ac075c9
Ashley Chung <ashleychung@ashley-home.local>
2025-07-10 15:33:05 +0100

    style: safe area panel

---
+++ Assets/Scripts/SafeArea.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(RectTransform))]
+public class SafeAreaFitter : MonoBehaviour
+{
+    private RectTransform panel;
+    private Rect lastSafeArea = new Rect(0, 0, 0, 0);
+
+    void Awake()
+    {
+        panel = GetComponent<RectTransform>();
+        ApplySafeArea();
+    }
+
+    void Update()
+    {
+        // check every frame to handle (any) changes
+        if (lastSafeArea != Screen.safeArea)
+        {
+            ApplySafeArea();
+        }
+    }
+
+    private void ApplySafeArea()
+    {
+        lastSafeArea = Screen.safeArea;
+
+        // convert safe area rectangle from screen to normalised viewport space
+        Vector2 anchorMin = lastSafeArea.position;
+        Vector2 anchorMax = lastSafeArea.position + lastSafeArea.size;
+
+        anchorMin.x /= Screen.width;
+        anchorMin.y /= Screen.height;
+        anchorMax.x /= Screen.width;
+        anchorMax.y /= Screen.height;
+
+        // apply normalised anchors
+        panel.anchorMin = anchorMin;
+        panel.anchorMax = anchorMax;
+
+        Debug.Log($"Safe Area Applied: Min({anchorMin.x}, {anchorMin.y}) Max({anchorMax.x}, {anchorMax.y})");
+    }
+}
