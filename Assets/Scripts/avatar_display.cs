0d1ee594a575b754ae0bcddc587c266551775834
Ashley Chung <ashleychung@ashley-home.local>
2025-07-24 02:04:16 +0100

    feat: avatar display

--- Assets/Scripts/avatar_display.cs
+++ Assets/Scripts/avatar_display.cs
@@ -1,26 +1,48 @@
 using UnityEngine;
 using AvatarSDK.MetaPerson.Loader;
+using System.Threading.Tasks;
 
 public class AvatarDisplay : MonoBehaviour
 {
     public MetaPersonLoader metaPersonLoader;
+    public OrbitControls cameraControls;
 
-    void Start()
+    async void Start()
     {
-        if (metaPersonLoader == null)
+        if (metaPersonLoader == null || cameraControls == null)
         {
-            Debug.LogError("metaperson loader not assigned in inspector");
+            Debug.LogError("metaperson loader or camera controls not assigned in inspector");
             return;
         }
 
-        // RETRIEVE url from persistent avatar manager
+        // FETCH url from persistent avatar manager
         string avatarUrl = AvatarManager.Instance.CurrentAvatarUrl;
 
         // check if valid, then load model
         if (!string.IsNullOrEmpty(avatarUrl))
         {
             Debug.Log("loading model from url: " + avatarUrl);
-            metaPersonLoader.LoadModel(avatarUrl);
+
+            bool isLoaded = await metaPersonLoader.LoadModelAsync(avatarUrl);
+
+            if (isLoaded && metaPersonLoader.transform.childCount > 0)
+            {
+                // -- 1. FETCH newly loaded avatar, CREATE pivot --
+                GameObject avatarObject = metaPersonLoader.transform.GetChild(0).gameObject;
+                GameObject avatarPivot = new GameObject("AvatarPivot");
+                avatarPivot.transform.position = avatarObject.transform.position; // match initial position
+
+                avatarObject.transform.SetParent(avatarPivot.transform); // set pivot
+
+                // -- 2. move model down relative to new pivot --
+                avatarObject.transform.localPosition = new Vector3(0, -0.9f, 0);
+
+                // -- 3. rotate new pivot (forward face avatar) --
+                avatarPivot.transform.rotation = Quaternion.Euler(0, 180, 0);
+
+                // -- 4. set camera controls --
+                cameraControls.target = avatarPivot.transform;
+            }
         }
         else
         {
